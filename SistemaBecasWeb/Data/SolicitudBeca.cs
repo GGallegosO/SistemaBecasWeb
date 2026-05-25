af8e1ee5b3eea5af0a4aa24b857e29fd60358d83
Gust <contacto.gustavoggmail.com>
2026-05-25 13:01:09 -0400

    Configuración de Entity Framework, Modelo y conexión a SQL Server

---
+++ SistemaBecasWeb/Data/SolicitudBeca.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SistemaBecasWeb.Models;
+
+namespace SistemaBecasWeb.Data
+{
+    public class AppDbContext : DbContext
+    {
+        // El constructor que recibe la configuración desde Program.cs
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        // Aquí le decimos que nuestra clase SolicitudBeca representa la tabla SolicitudesBeca
+        public DbSet<SolicitudBeca> Solicitudes { get; set; }
+    }
+}
